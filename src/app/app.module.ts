7b56eb0c7da3aeed78006a15cdc8f9ca86823577
Daniel Oliveira Araújo <danieloliveira14@gmail.com>
2025-02-25 18:24:35 -0300

    instalando firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
   declarations: [
@@ -10,10 +15,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule
+
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideFirebaseApp(() => initializeApp({"projectId":"devops-uece","appId":"1:415178932814:web:a85da54d816710e5edb91f","storageBucket":"devops-uece.firebasestorage.app","apiKey":"AIzaSyCbtNPxrqYDdP-W-ciTZY4JkNMbP6DBSa0","authDomain":"devops-uece.firebaseapp.com","messagingSenderId":"415178932814"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase())
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyCbtNPxrqYDdP-W-ciTZY4JkNMbP6DBSa0",
+    authDomain: "devops-uece.firebaseapp.com",
+    projectId: "devops-uece",
+    storageBucket: "devops-uece.firebasestorage.app",
+    messagingSenderId: "415178932814",
+    appId: "1:415178932814:web:a85da54d816710e5edb91f"
+  }
+};
